1c83bf8c7045f092f7b7dba349c40acb00bc65fd
jenanos <jens.osberg@gmail.com>
2022-08-03 18:51:51 +0200

    add three fiber

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const withTM = require('next-transpile-modules')(['three'])
+module.exports = withTM()
