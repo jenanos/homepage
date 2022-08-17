63753c54f22bcacd0451798f62ebe5c8d2aa4581
jenanos <jens.osberg@gmail.com>
2022-08-17 20:17:36 +0200

    add first own model

--- components/SpaceBackground.tsx
+++ components/SpaceBackground.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
 import { Environment, OrbitControls, Text3D } from '@react-three/drei';
+import { Model } from './Model';
 
 function SpaceBackground() {
     return (
         <Canvas>
-            <ambientLight intensity={0.1} />
-            <directionalLight color="red" position={[0, 0, 5]} />
-            <Text3D font={'/assets/Roboto_Medium_Regular.json'} position={[-5, 4, 3]}>
+            <ambientLight intensity={0.03} />
+            <pointLight color="orange" intensity={1} position={[-11.4, 2.8, 8.8]} distance={8} />
+            <pointLight color="orange" intensity={1} position={[-3.2, 3, 4.5]} distance={8} />
+            <pointLight color="orange" intensity={1} position={[3.6, 4, -1]} distance={2} />
+            <pointLight color="white" intensity={1} position={[12.3, 1.9, 8.9]} distance={8} />
+            <Text3D font={'/assets/Roboto_Medium_Regular.json'} position={[-5, 10, 10]}>
                 Hei, jeg er Jens!
                 <meshNormalMaterial />
             </Text3D>
+            <Model />
             <OrbitControls />
             <Environment
                 files={[
